60014917f26602f3f6ae34fb74fe2a57d512af7f
Tanuj2004 <22egjcs811@gitjaipur.com>
2024-09-03 15:17:26 +0530

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,7 +1,6 @@
 public final class Constants
 {
   
-
     public static final String PROMPT_MESSAGE = "Enter a string:";
     public static final String INTEGER = "Enter a integer:";
     public static final String COMPRESSED_MESSAGE = "Compressed string: ";
@@ -9,5 +8,6 @@ public final class Constants
     public static final String[] ONE_TO_NINETEEN = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
     public static final String[] TENS = {"", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     public static final String[] THOUSANDS = {"", "thousand", "million", "billion"};
-
+    public static final String LONG= "Length of the longest substring without repeating characters: ";
+    public static final String LONGESTSTRING = "Longest substring without repeating characters: ";
 }
